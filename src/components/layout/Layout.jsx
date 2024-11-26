990913170539b649fe0128f16e9d9e9f4b85e767
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2024-11-27 00:27:24 +0200

    add navigate for logout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ user, setUser }) {
   const handleLogout = (e) => {
     e.preventDefault();
     setUser([]);
-    navigate("/");
+    navigate("/", { replace: true });
   };
   return (
     <div className="page-container">
